b2d8d39709bddb7504f3a12cd4cce8ed535377a8
Ant2357 <ryu4979@gmail.com>
2023-12-23 01:18:40 +0900

    fix: 背景がはみ出る問題の修正とAA修正

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import "pure-snow.js/style.css";
+import "./style.css";
+
+import "./features/pure-snow/styles/style.css";
 import { createSnow, showSnow } from "pure-snow.js";
 
+import "./features/xmas/styles/xmas.css";
 import { displayNextAsciiArt } from "./features/xmas/scripts/xmas";
 
 // 雪を降らせる
